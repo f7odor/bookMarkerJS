c5a1a287368294cdc32f7b88edeb51709fd8b869
alexey <dust1@bk.ru>
2017-09-01 15:58:43 +0300

    add f_fetchBookmarks && f_delete and clear

--- js/main.js
+++ js/main.js
@@ -40,6 +40,58 @@ function saveBookmark(e){
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	}
 
+  //Cleat Form
+  document.getElementById('myForm').reset();
+
+  // Re-fetch bookmarks
+  fetchBookmarks();
+
+
 	// Prevent form from submitting
 	e.preventDefault();
 }
+
+// Delete bookmark
+function deleteBookmark(url){
+  //Get bookmarks from LocalStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+  //Loop throught bookmarks
+  for(var i = 0; i < bookmarks.length; i++){
+    if(bookmarks[i].url == url){
+      //Remove from array
+      bookmarks.splice(i, 1);
+    }
+  }
+  // Re-set back to LocalStorage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  // Re-fetch bookmarks
+  fetchBookmarks();
+
+}
+
+// Fetch bookmarks
+function fetchBookmarks(){
+	// Get bookmarks from localStorage 
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+	// Get output id
+	var bookmarksResults = document.getElementById('bookmarksResults');
+
+	// Build output
+	bookmarksResults.innerHTML = '';
+	for(var i = 0; i < bookmarks.length; i++){
+		var name = bookmarks[i].name;
+		var url = bookmarks[i].url;
+
+		bookmarksResults.innerHTML += '<div class="well well-lg">'+
+                                  '<h3>'+name+
+                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" target="_blank" href="#">Delete</a> ' 
+                                  '</h3>'+
+                                  '</div>';
+		}
+	}
+
+// Validation Form
